a053e6932a7e30800f9dee331e7ef669e84e119d
Prabhat <87269080+Prabhatsingh001@users.noreply.github.com>
2025-07-06 23:02:49 +0530

    made navbar more responsive

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
+import { Menu, X } from 'lucide-react';
+
+
 export default Navigation = ({ onContactClick, activeSection }) => {
   const scrollToSection = (sectionId) => {
     document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
@@ -12,14 +15,21 @@ export default Navigation = ({ onContactClick, activeSection }) => {
         : 'text-gray-700 hover:text-pink-500'
     }`;
 
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="fixed top-0 w-full bg-black/90 backdrop-blur-md z-50 border-b border-gray-800">
       <div className="max-w-6xl mx-auto px-6 py-4">
         <div className="flex justify-between items-center">
           <div className="flex items-center space-x-2">
             <div className="w-8 h-8 bg-black rounded"><img src="../../QunatNum.svg"/></div>
-            <span className="font-bold text-xl">QuantNum</span>
+            <a href="/" className="text-xl font-bold hover:opacity-80 transition duration-200">
+            <span className="text-red-500">Quant</span>
+            <span className="text-white">Num</span>
+            </a>
           </div>
+
+          {/* desktop menu */}
           <div className="hidden md:flex space-x-8">
             <button onClick={() => scrollToSection('events')} className={linkClasses('events')}>
               Events
@@ -35,12 +45,38 @@ export default Navigation = ({ onContactClick, activeSection }) => {
             </button>
             <button onClick={onContactClick} className={linkClasses('footer')}>
 
-          <Logo showText={true} className="w-10 h-10" />
+          {/* <Logo showText={true} className="w-10 h-10" /> */}
          
               Contact
             </button>
           </div>
+
+          <div className='md:hidden'>
+            <button onClick={()=>setIsOpen(!isOpen)}>
+              {isOpen ? <X size={28} color='white' /> : <Menu size={28} color='white' />}
+            </button>
+          </div>
         </div>
+
+        {isOpen && (
+          <div className="flex flex-col md:hidden space-y-4 mt-4">
+            <button onClick={() => scrollToSection('events')} className= {linkClasses('events')}>
+              Events
+            </button>
+            <button onClick={() => scrollToSection('Announces')} className={linkClasses('Announces')}>
+              Announcements
+            </button>
+            <button onClick={() => scrollToSection('achievements')} className={linkClasses('achievements')}>
+              Achievements
+            </button>
+            <button onClick={() => scrollToSection('resources')} className={linkClasses('resources')}>
+              Resources
+            </button>
+            <button onClick={onContactClick} className={linkClasses('footer')}>         
+              Contact
+            </button>
+          </div>
+        )}
       </div>
     </nav>
   );
